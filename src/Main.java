62df388b061cba2d3319d99a49499734a26311b2
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-03 20:08:44 +0300

    Наследование

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
         public static void main(String[] args) {
-            Car car = new Car("car1", 4);
-            Car car2 = new Car("car2", 4);
+            Car [] cars = {
+                    new Car ("car1", 4),
+                    new Car ("car2", 4),
+            };
+            Truck [] trucks = {
+                    new Truck("truck1", 6),
+                    new Truck("truck2", 8),
+            };
+            Bicycle[] bicycles = {
+                    new Bicycle("bicycle1", 2),
+                    new Bicycle("bicycle2", 2),
+            };
 
-            Truck truck = new Truck("truck1", 6);
-            Truck truck2 = new Truck("truck2", 8);
+            Vehicle [] vehicles = new Vehicle[cars.length+ trucks.length+ bicycles.length];
+                for (int i = 0; i < cars.length; i++) {
+                    vehicles[i] = cars[i];
+                }
+                 for (int i = 0; i < trucks.length; i++) {
+                        vehicles[i + cars.length] = trucks[i];
+                    }
+                    for (int i = 0; i < bicycles.length; i++) {
+                        vehicles[i + cars.length + trucks.length] = bicycles[i];
+                    }
 
-            Bicycle bicycle = new Bicycle("bicycle1", 2);
-            Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+                }
+
+
 
             ServiceStation station = new ServiceStation();
             station.check(car, null, null);
@@ -17,4 +38,4 @@ public class Main {
             station.check(null, null, truck);
             station.check(null, null, truck2);
         }
-    }
+
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -23,14 +23,6 @@ public class Vehicle {
         this.wheelsCount = wheelsCount;
     }
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
-    public void checkTrailer() {
-        System.out.println("Проверяем прицеп");
-    }
+
 }
 
