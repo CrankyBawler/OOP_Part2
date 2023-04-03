c77f9f34d0ab09c9dcf334f64f00c7a989fe26c3
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-03 19:11:11 +0300

    Наследование

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,30 +1,5 @@
-public class Bicycle {
-
-    private String modelName;
-    private int wheelsCount;
-
-    public Bicycle(String modelName, int wheelsCount) {
-        this.modelName = modelName;
-        this.wheelsCount = wheelsCount;
-    }
-
-    public String getModelName() {
-        return modelName;
-    }
-
-    public void setModelName(String modelName) {
-        this.modelName = modelName;
-    }
-
-    public int getWheelsCount() {
-        return wheelsCount;
-    }
-
-    public void setWheelsCount(int wheelsCount) {
-        this.wheelsCount = wheelsCount;
-    }
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
+public class Bicycle extends Vehicle {
+    public Bicycle(String modelName, int wheelsCount){
+        super(modelName, wheelsCount);
+        }
 }
